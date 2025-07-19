73965ff744fd790ca56eb79a6867144491d18910
Sergey Bovin <snowbovin@gmail.com>
2025-07-19 12:33:35 +0300

    feat(catalogUI): добавлен компонент каталога, его стили и моки под него

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes } from 'react-router-dom';
 import './styles/index.css';
+import Catalog from '@/widgets/Catalog/Catalog';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes></Routes>
+      {/* Отображаем каталог в обход роутов, если нужно авторизоваться - пишем isAuthenticated={true} */}
+      <Catalog isAuthenticated={false} />
     </BrowserRouter>
   );
 }
