14b1734f1a58c7fbcf5a92781cdf72894b8be4e2
kiipiru <bigfatmouther@gmail.com>
2025-07-27 15:08:57 +0400

    feat(store): добавлен и настроен store из RTK

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './app/App';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './app/providers/store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     </HelmetProvider>
   </React.StrictMode>,
 );
