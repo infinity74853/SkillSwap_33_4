22c77e84263a6d412ad44abbe8523474740c14b4
kiipiru <bigfatmouther@gmail.com>
2025-07-29 02:56:26 +0400

    feat(ui): добавлен компонент textInput

---
+++ src/shared/ui/textInput/textInput.tsx
@@ -0,0 +1,50 @@
+import { FC } from 'react';
+import styles from './textInput.module.css';
+
+export type TextInputProps = {
+  className?: string;
+  icon?: string;
+  id: string;
+  title: string;
+  placeholder: string;
+  onInputClick?: () => void;
+  onInputBlur?: () => void;
+  value?: string;
+  onChange: (value: string) => void;
+  error?: string;
+  type?: 'text' | 'email' | 'password' | 'tel' | 'number' | 'url';
+};
+
+export const TextInput: FC<TextInputProps> = ({
+  id,
+  title,
+  placeholder,
+  onInputClick,
+  value,
+  className,
+  icon,
+  onInputBlur,
+  onChange,
+  error,
+  type,
+}) => {
+  return (
+    <div className={`${styles.container} ${className}`}>
+      <label className={styles.label} htmlFor={id}>
+        {title}
+      </label>
+      <input
+        type={type}
+        className={`${styles.input} ${icon ? styles.inputWithIcon : ''} ${error ? styles.inputError : ''}`}
+        id={id}
+        placeholder={placeholder}
+        onClick={onInputClick}
+        value={value}
+        style={icon ? { backgroundImage: `url(${icon})` } : undefined}
+        onBlur={onInputBlur}
+        onChange={e => onChange(e.target.value)}
+      />
+      {error && <span className={styles.error}>{error}</span>}
+    </div>
+  );
+};
