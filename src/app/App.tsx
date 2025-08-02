d795517e363bef85f210faead6018d669a74fba4
i-nikotin-THEGAME <i_nikotin@thegame.design>
2025-08-02 13:07:58 +0300

    feat: вёрстка уведомлений о заявках на обмен (MVP)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import { MainLayout } from '@/widgets/Layout/MainLayout';
 import ProfileDetailsPage from '@/pages/profileDetails/ProfileDetailsPage';
-import TextTestComponent from '@/widgets/TestComponent/TestComponent';
-import Catalog from '@/widgets/catalog/catalog';
+//import TextTestComponent from '@/widgets/TestComponent/TestComponent';
+//import Catalog from '@/widgets/catalog/catalog';
 import './styles/index.css';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { Suspense, useEffect } from 'react';
@@ -10,7 +10,7 @@ import { useDispatch } from '@/services/store/store';
 import { initializeLikes } from '@/services/slices/likeSlice';
 import { fetchCatalog } from '@/services/slices/catalogSlice';
 import { SuccessModal } from '@/features/successModal/successModal';
-import { RegistrationForms } from '@/features/registrationForms/registrationForms';
+import { RegistrationForms } from '@/features/RegistrationForms/registrationForms';
 import { ErrorPage } from '@/pages/ErrorPage/ErrorPage';
 import SkillPage from '@/pages/skillPage/skillPage';
 import { CatalogPage } from '@/pages/catalogPage/catalogPage';
@@ -42,10 +42,7 @@ function App() {
           {/* index-маршрут для корневого пути "/" */}
           <Route index element={<CatalogPage />} />
 
-          <Route
-            path="/profile/details"
-            element={<ProfileDetailsPage />}
-          />
+          <Route path="/profile/details" element={<ProfileDetailsPage />} />
           <Route
             path="/profile/favorites"
             element={
