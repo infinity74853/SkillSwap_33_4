a036d524e1c1a46c142068a61acaa8f6cb101e8e
kiipiru <bigfatmouther@gmail.com>
2025-08-03 10:51:33 +0400

    feat(skillsSlice): создан slice для получения всех категорий навыков

---
+++ src/services/slices/skillsSlice.ts
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSelector, createSlice } from '@reduxjs/toolkit';
+import { getSkillsApi } from '@/api/skillSwapApi';
+import { Skill } from '@/entities/skill/model/types';
+
+type SkillsState = {
+  skills: Array<Skill>;
+  loading: boolean;
+  error: string | undefined;
+};
+
+const initialState: SkillsState = {
+  skills: [],
+  loading: false,
+  error: undefined,
+};
+export const getSkills = createAsyncThunk('skills/getAll', async () => getSkillsApi());
+
+const skillsSlice = createSlice({
+  name: 'skills',
+  initialState,
+  reducers: {},
+  selectors: { getSkillsSelector: state => state.skills },
+  extraReducers: builder => {
+    builder
+      .addCase(getSkills.pending, state => {
+        state.loading = true;
+        state.error = undefined;
+      })
+      .addCase(getSkills.rejected, state => {
+        ((state.loading = false),
+          (state.error = new Error('Не удалось загрузить данные о навыках').message));
+      })
+      .addCase(getSkills.fulfilled, (state, action) => {
+        state.skills = action.payload.data;
+      });
+  },
+});
+
+export const { getSkillsSelector } = skillsSlice.selectors;
+export const skillsReducer = skillsSlice.reducer;
+
+export const getCategoriesSelector = createSelector(getSkillsSelector, skills =>
+  Array.from(new Set(skills.map(skill => skill.category))),
+); // отдельный селектор с мемоизацией
