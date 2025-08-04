c050d7646c4f2a2e273ddf41f974b40d0034e3cc
Infinity <igor74853@yandex.ru>
2025-08-04 17:35:57 +0300

    feat: добавлено меню уведомлений

--- src/features/auth/ui/UserPanel/UserPanel.tsx
+++ src/features/auth/ui/UserPanel/UserPanel.tsx
@@ -1,24 +1,42 @@
 import { useAuth } from '@/features/auth/context/AuthContext';
 import { useClickOutside } from '@/shared/hooks/useClickOutside';
+import { useState, useRef, useEffect } from 'react';
+import { NotificationMenu } from '../NotificationMenu/NotificationMenu';
 import styles from './UserPanel.module.css';
-import { useState, useRef } from 'react';
 
 export const UserPanel = () => {
   const { user, logout } = useAuth();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isNotificationsOpen, setIsNotificationsOpen] = useState(false);
   const panelRef = useRef<HTMLDivElement>(null);
 
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-  const closeMenu = () => setIsMenuOpen(false);
+  const toggleNotifications = () => setIsNotificationsOpen(!isNotificationsOpen);
+  const closeAll = () => {
+    setIsMenuOpen(false);
+    setIsNotificationsOpen(false);
+  };
 
-  useClickOutside(panelRef, closeMenu);
+  useClickOutside(panelRef, closeAll);
+
+  useEffect(() => {
+    if (isNotificationsOpen || isMenuOpen) {
+      document.body.classList.add('menu-open');
+    } else {
+      document.body.classList.remove('menu-open');
+    }
+
+    return () => {
+      document.body.classList.remove('menu-open');
+    };
+  }, [isNotificationsOpen, isMenuOpen]);
 
   return (
     <div className={styles.userPanel} ref={panelRef}>
       <div className={styles.iconsContainer}>
-        <div className={styles.iconWrapper}>
+        <div className={styles.iconWrapper} onClick={toggleNotifications}>
           <span className={styles.notificationIcon}></span>
-          <span className={styles.notificationBadge}></span>
+          <span className={styles.notificationBadge}>4</span>
         </div>
         <div className={styles.iconWrapper}>
           <span className={styles.likeIcon}></span>
@@ -43,6 +61,8 @@ export const UserPanel = () => {
           </button>
         </div>
       )}
+
+      <NotificationMenu isOpen={isNotificationsOpen} />
     </div>
   );
 };
