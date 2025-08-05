eb6bf0c7d7e154826fe32a358e60392167190c7d
Geo1251 <george.nozdrin@yandex.ru>
2025-08-05 14:16:16 +0300

    fix: jest fix

--- jest.config.ts
+++ jest.config.ts
@@ -2,15 +2,15 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 
 const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
-
   testEnvironment: 'jest-environment-jsdom',
-
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
 
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',
       {
+        compiler: 'ttypescript',
+
         tsconfig: 'tsconfig.test.json',
       },
     ],
@@ -18,14 +18,13 @@ const config: JestConfigWithTsJest = {
 
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+
+    '\\.(svg)$': '<rootDir>/__mocks__/svg.js',
   },
 
   testPathIgnorePatterns: ['/node_modules/'],
-
   testMatch: ['**/__tests__/**/*.test.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
